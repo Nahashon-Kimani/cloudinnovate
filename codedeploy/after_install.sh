687e29a13a3546d7f50950ecc33796931506f779
Nahashon-Kimani <nahashonmbuci@gmail.com>
2026-03-26 16:00:54 +0300

    added appspec

--- codedeploy/after_install.sh
+++ codedeploy/after_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# Set correct permissions for Apache2
-chown -R www-data:www-data /var/www/html
+# # Set correct permissions for Apache2
+# chown -R www-data:www-data /var/www/html
 chmod -R 755 /var/www/html
+echo "Set directory permissions to 755"
+
+echo "Permissions set successfully"
 
 chmod +x codedeploy/*.sh
